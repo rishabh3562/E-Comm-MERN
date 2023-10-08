f45412f801375fb9b9fcd04b7b478cdbc441b898
Rishabh Dubey <rishabh.dubey.cs21@ggits.net>
2023-10-08 22:57:52 +0530

    .

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { getAlltProducts, getProductDetails, updateProduct, deleteProduct, createProduct } = require("../controllers/productController");
+// const {isAuthenticatedUser} = require("../middleware/auth");
+const {}=require("../middleware/auth")
 router.route("/products").get(getAlltProducts);
 router.route("/products/new").post(createProduct)
 
